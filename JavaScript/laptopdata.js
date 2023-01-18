243512d13a8ba388372baeb5de47192c74fef967
UmtWebCoop <119837815+UmtWebCoop@users.noreply.github.com>
2023-01-18 21:58:25 +0800

    Add files via upload

---
+++ JavaScript/laptopdata.js
@@ -0,0 +1,78 @@
+var product = [{
+    "id": 0,
+    "title": "",
+    "price": "",
+    "brand": "",
+    "cpu" : "",
+    "gpu" : "",
+    "storage" : "",
+    "ram" : "",
+    "rating" : "",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+}, {
+    "id": 1,
+    "title": "ROG Zephyrus G14 GA401",
+    "price": "RM 4,699",
+    "brand": "Asus Republic of Gamers",
+    "cpu" : "Ryzen™ 9 5900HS",
+    "gpu" : "NVIDIA RTX™ 3060",
+    "storage" : "1TB + 1TB M.2 NVMe™ PCIe® 3.0 Performance SSD",
+    "ram" : "8GB DDR5",
+    "rating" : "4.2 of 5 stars",
+    "image": "images/Asusrog.jpg"
+}, {
+    "id": 2,
+    "title": "Legion 5i",
+    "price": "RM 6,250",
+    "brand": "Lenovo",
+    "cpu" : "12th Generation Intel® Core™ i7-12700H Processor (E-cores up to 3.50 GHz P-cores up to 4.70 GHz)",
+    "gpu" : "NVIDIA GeForce® RTX™ 3060 6GB GDDR6",
+    "storage" : "1 TB SSD M.2 2280 PCIe Gen4/4",
+    "ram" : "32 GB (2 x 16 GB) SO-DIMM DDR5 4800MHz",
+    "rating" : "4.5 of 5 stars",
+    "image": "images/legion.jpg"
+}, {
+    "id": 3,
+    "title": "Zenbook Duo 14 (UX482)",
+    "price": "RM 4,999",
+    "brand": "Asus",
+    "cpu" : "Intel® Core™ i5-1135G7 Processor 2.4 GHz (8M Cache, up to 4.2 GHz, 4 cores)",
+    "gpu" : "Intel Iris Xᵉ Graphics",
+    "storage" : "512 GB M.2 NVMe PCIe 3.0 SSD",
+    "ram" : "8GB LPDDR4X",
+    "rating" : "4.2 of 5 stars",
+    "image": "images/asusduo.jpg"
+}, {
+    "id": 4,
+    "title": "IdeaPad 5i Pro",
+    "price": "RM 3,984",
+    "brand": "Lenovo",
+    "cpu" : "12th Generation Intel® Core™ i7-1260P Processor (E-cores up to 3.40 GHz P-cores up to 4.70 GHz)",
+    "gpu" : "NVIDIA GeForce® RTX™ 2050 4GB GDDR6",
+    "storage" : "512 GB SSD M.2 2280 PCIe Gen4/4",
+    "ram" : "16 GB Soldered LPDDR5 4800MHz",
+    "rating" : "3.8 of 5 stars",
+    "image": "images/ideapad5i.jpg"
+}, {
+    "id": 5,
+    "title": "Predator Helios 300",
+    "price": "RM 5,149",
+    "brand": "Acer",
+    "cpu" : "Intel i5-9300H",
+    "gpu" : "NVIDIA GeForce RTX 2060 6GB GDDR6",
+    "storage" : "512 GB SSD",
+    "ram" : "8GB DDR4 SDRAM",
+    "rating" : "4.4 of 5 stars",
+    "image": "images/predator300.jpg"
+}, {
+    "id": 6,
+    "title": "Predator Helios 700",
+    "price": "RM 5,149",
+    "brand": "Acer",
+    "cpu" : "Intel® Core™ i9 (i9-10980HK 2.40 GHz Octa-core(8 Core™) 10th Gen)",
+    "gpu" : "NVIDIA GeForce® RTX™ 2080 SUPER (8 GB GDDR6)",
+    "storage" : "1 TB SSD",
+    "ram" : "16 GB DDR4 SDRAM",
+    "rating" : "3.8 of 5 stars",
+    "image": "images/predator700.jpg"
+}]
